c22aeff30402d9e80d53e2386472bce5bc8f8483
sayemmy007 <sayemmy007@gmail.com>
2023-04-08 16:40:20 +0530

    jasper

---
+++ src/main/java/com/example/demo/JasperReportApplication.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Employee;
+import com.example.demo.repository.EmployeeRepository;
+import com.example.demo.service.ReportService;
+
+import net.sf.jasperreports.engine.JRException;
+
+
+@SpringBootApplication
+@RestController
+public class JasperReportApplication {
+    @Autowired
+    private EmployeeRepository repository;
+    @Autowired
+    private ReportService service;
+
+    @GetMapping("/getEmployees")
+    public List<Employee> getEmployees() {
+
+        return repository.findAll();
+    }
+
+    @GetMapping("/report/{format}")
+    public String generateReport(@PathVariable String format) throws FileNotFoundException, JRException {
+        return service.exportReport(format);
+    }
+	public static void main(String[] args) {
+		SpringApplication.run(JasperReportApplication.class, args);
+	}
+
+}
